17c920dcffc74978da44158ea53363938637c8cb
spaceh3ad <jan.kwiatkowski@studenci.wsksim.edu.pl>
2021-10-24 22:55:37 +0200

    added unit test

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Users can enter lottery with ETH based on a USD fee
+2. An admin will chose when lottery is over
+3. The lottery will select a random winner
+
+How do we want to test it?
+
+1. `mainnet-fork`
+2. `development` with mocks
---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,85 @@
+from brownie import (
+    accounts,
+    network,
+    config,
+    MockV3Aggregator,
+    VRFCoordinatorMock,
+    LinkToken,
+    Contract,
+    interface,
+)
+
+FORKED_LOCAL_ENVIROMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIROMENTS = ["development", "ganache-local"]
+
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIROMENTS
+    ):
+        return accounts[0]
+
+    return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+}
+
+
+def get_contract(contract_name):
+    """this function ll grab contract addresses from brownie config if defined,
+    otherwise it will deploy a mock contract and return mock contract
+
+        Args:
+            contrac_name [string]
+
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently
+            deployed version of this contract
+
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIROMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+
+    else:
+        contract_address = config["networks"][network.show_actice()][contract_name]
+        contract = Contract.from_abi(
+            contract_type._name, contract_address, contract_type.abi
+        )
+    return contract
+
+
+DECIMALS = 8
+INITIAL_VALUE = 20000000000
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
+    account = get_account()
+    MockV3Aggregator.deploy(decimals, initial_value, {"from": account})
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Deployed")
+
+
+def fund_with_link(
+    contract_address, account=None, link_token=None, amount=100000000000000000
+):
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
+    # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
+    tx.wait(1)
+    print("Funded contract with LINK!")
+    return tx
